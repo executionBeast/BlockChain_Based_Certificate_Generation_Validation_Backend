118e054b70ebb1715d80ec126b78f83e3c5d4b14
executionBeast <raos92249@gmail.com>
2024-10-18 10:24:28 +0530

    commiting

---
+++ src/controllers/getAllUsers.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.models.js";
+
+const getAllUsers = async (req,res)=>{
+    try{
+        const userdata = await User.find({usertype:'student'})
+        console.log(userdata)
+        res.status(200).json(userdata);
+
+    }
+    catch(err){
+        console.log("Error While Getting User (student)",err)
+        res.status(500).send({
+            err:err,
+            msg:"Unable to Get Users"
+        })
+    }
+    
+}
+
+export default getAllUsers;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import express from "express";
 const router = express.Router();
 import createUser from "../controllers/createUser.controller.js";
 import sendUserId from "../controllers/login.controller.js";
+import getAllUsers from "../controllers/getAllUsers.controller.js";
 
 //USER (student)
 router.post("/user",createUser)
 router.post("/login",sendUserId)
+router.get("/users",getAllUsers)
 
 //ISSUER
 router.post("/issuer",createUser)
